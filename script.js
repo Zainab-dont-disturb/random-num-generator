a0843f836e6ff9506d401ef8e978f102afe5cec6
Zainab-dont-disturb <zainabadaranijo111@gmail.com>
2024-11-24 13:58:47 +0000

    done

---
+++ script.js
@@ -0,0 +1,8 @@
+// Generate a random number between 0 and 100
+let randomnum = Math.floor(Math.random() * 101);
+
+// Print the random number
+console.log(randomnum);
+
+// Or display it in an HTML element
+document.getElementById("randomnum").innerHTML = randomnum;
